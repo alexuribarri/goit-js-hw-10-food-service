6719236b9fc1a34cd600ac370f98546b2a8e6bb6
alexuribarri <alex@uribarri.es>
2021-04-26 20:46:38 +0200

    testing commit

--- src/index.js
+++ src/index.js
@@ -1,4 +1,13 @@
 import './styles.css';
-import 'handlebars';
-var template = Handlebars.compile('Handlebars <b>{{doesWhat}}</b>')
-console.log(template({doesWhat:'Rocks'}))
+import Handlebars from 'handlebars';
+
+const body = document.querySelector('body');
+console.log(body);
+body.insertAdjacentHTML('beforeend', '<h1>Hello World</h1>');
+
+// body.innerHTML('<h1>Hello World</h1>');
+
+console.log(Handlebars);
+
+var template = Handlebars.compile('Handlebars <b>{{doesWhat}}</b>');
+console.log(template({ doesWhat: 'Rocks' }));
