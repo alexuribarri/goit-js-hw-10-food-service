6544bff08b753bc138e743af0c532fe3c10884c6
alexuribarri <alex@uribarri.es>
2021-04-27 09:33:09 +0200

    building functions

--- src/item.js
+++ src/item.js
@@ -1,7 +1,31 @@
 import item from './templates/item.hbs';
-//console.log(item({ hello: 'zyuzya' }));
-document
-  .querySelector('img[src]')
-  .insertAdjacentHTML('beforeend', item({ imageUrl: 'https://www.go-cook.ru/wp-content/uploads/2014/11/magribskij-tomatnyj-sup.jpg' }));
+import menu from './menu.json';
 
+const itemsRecall = (src, alt, cardName, price, cardDescr, tagList) => {
+  // item({ });
 
+  document
+    .querySelector('.js-menu')
+    .insertAdjacentHTML(
+      'beforeend',
+      item({ imageUrl: src, imageAlt: alt, itemCardName: cardName }),
+    );
+};
+
+itemsRecall(
+  'https://www.go-cook.ru/wp-content/uploads/2014/11/magribskij-tomatnyj-sup.jpg',
+  0,
+  0,
+  0,
+  0,
+  0,
+);
+
+// const menuItems = menu.map(menuItem => console.log(menuItem.description));
+
+// const imgUrl = {
+//   imageUrl:
+//     'https://www.go-cook.ru/wp-content/uploads/2014/11/magribskij-tomatnyj-sup.jpg',
+// };
+
+// console.log(imgUrl);
