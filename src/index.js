c60c107e3666778eba895100fa641a52299a8d23
Alex Uribarri <alex@uribarri.es>
2021-04-28 19:32:23 +0200

    test2

--- src/index.js
+++ src/index.js
@@ -2,13 +2,12 @@ import './styles.css';
 import Handlebars from 'handlebars';
 import './item';
 
-const body = document.querySelector('body');
-console.log(body);
-body.insertAdjacentHTML('beforeend', '<h1>Hello World</h1>');
+//const body = document.querySelector('body');
 
-// body.innerHTML('<h1>Hello World</h1>');
+//body.insertAdjacentHTML('beforeend', '<h1>Hello World</h1>');
 
-console.log(Handlebars);
 
-var template = Handlebars.compile('Handlebars <b>{{doesWhat}}</b>');
-console.log(template({ doesWhat: 'Rocks' }));
+//console.log(Handlebars);
+
+//var template = Handlebars.compile('Handlebars <b>{{doesWhat}}</b>');
+//console.log(template({ doesWhat: 'Rocks' }));
--- src/item.js
+++ src/item.js
@@ -1,5 +1,7 @@
 import item from './templates/item.hbs';
-console.log(item({ hello: 'zyuzya' }));
+//console.log(item({ hello: 'zyuzya' }));
 document
-  .querySelector('body')
-  .insertAdjacentHTML('afterbegin', item({ hello: 'zyuzya' }));
+  .querySelector('img[src]')
+  .insertAdjacentHTML('beforeend', item({ imageUrl: 'https://www.go-cook.ru/wp-content/uploads/2014/11/magribskij-tomatnyj-sup.jpg' }));
+
+
